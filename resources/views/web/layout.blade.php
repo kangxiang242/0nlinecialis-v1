e4174cbb06424af8546158257ae846a56fc87383
administrator <administrator@Im-lovin-it.local>
2026-07-15 14:41:10 +0800

    Harden frontend redirects and tracking URLs

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -61,8 +61,8 @@
         var is_mobile_domain = parseInt("{{ is_mobile()?"1":"0" }}");
     </script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="{{ assetv('static/swiper4/swiper.min.css') }}" />
+    <script src="{{ assetv('static/swiper4/swiper.min.js') }}"></script>
     <script>
         window.addEventListener('DOMContentLoaded', (event) => {
             const isPC = !/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
